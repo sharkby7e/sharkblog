45603b11df53de11442a82954d1df616cbbff4c1
sharkbyte <sgquin@gmail.com>
2022-11-13 15:05:21 -0800

    adjust index page and stylings

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "sharkby7e";
+export const SITE_TITLE = "sharkby7e 🦈";
 
 export const SITE_DESCRIPTION = "a cool website by sharkby7e written in Astro";
