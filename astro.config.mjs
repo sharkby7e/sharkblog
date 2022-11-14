d582d68124905bafd5a9fff70358f463496254b5
sharkbyte <sgquin@gmail.com>
2022-11-13 17:04:42 -0800

    remove tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
 });
